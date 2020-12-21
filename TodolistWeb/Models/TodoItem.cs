4b36336dfdee5acd2f3f8631a6a517cc3f5c96c6
Zajonc <fildagomez@seznam.cz>
2020-12-21 11:04:59 +0100

    1st commit

---
+++ TodolistWeb/Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodolistWeb.Models
+{
+    public class TodoItem
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; }
+
+    }
+}
